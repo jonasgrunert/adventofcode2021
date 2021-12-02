057e96a070778c3c4b7280c053ccea9097335321
jonasgrunert <jonas@grunert-berlin.de>
2021-12-02 10:53:35 +0100

    Refactor

--- 01_test.ts
+++ 01_test.ts
@@ -1,13 +1,13 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import { readFileToArray } from "./_util.ts";
-import { task1, task2 } from "./01.ts";
+import { getNumber } from "./_util.ts";
+import sol from "./01.ts";
 
-Deno.test("01 - Task 1", async () => {
-  const arr = await readFileToArray(Number);
-  assertEquals(task1(arr), 7);
+Deno.test(`${getNumber()} - Task 1`, async () => {
+  const res = await sol.result1;
+  assertEquals(res, 7);
 });
 
-Deno.test("01 - Task 2", async () => {
-  const arr = await readFileToArray(Number);
-  assertEquals(task2(arr), 5);
+Deno.test(`${getNumber()} - Task 2`, async () => {
+  const res = await sol.result2;
+  assertEquals(res, 5);
 });
--- 02_test.ts
+++ 02_test.ts
@@ -1,19 +1,13 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import { readFileToArray } from "./_util.ts";
-import { task1, task2 } from "./02.ts";
+import { getNumber } from "./_util.ts";
+import sol from "./02.ts";
 
-Deno.test("02 - Task 1", async () => {
-  const arr = await readFileToArray((val) => {
-    const [i, n] = val.split(" ");
-    return { i, n: Number(n) };
-  });
-  assertEquals(task1(arr), 150);
+Deno.test(`${getNumber()} - Task 1`, async () => {
+  const res = await sol.result1;
+  assertEquals(res, 150);
 });
 
-Deno.test("02 - Task 2", async () => {
-  const arr = await readFileToArray((val) => {
-    const [i, n] = val.split(" ");
-    return { i, n: Number(n) };
-  });
-  assertEquals(task2(arr), 900);
+Deno.test(`${getNumber()} - Task 2`, async () => {
+  const res = await sol.result2;
+  assertEquals(res, 900);
 });
