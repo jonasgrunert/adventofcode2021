86f0995c0d03f028473b14a710f77a94ac6d0bcb
jonasgrunert <jonas@grunert-berlin.de>
2022-01-10 09:53:06 +0100

    Day 25

---
+++ 25.ts
@@ -0,0 +1,41 @@
+import Solution from "./_util.ts";
+
+const task = new Solution(
+  (arr: string[]) => {
+    console.log();
+    let count = 0;
+    let map = arr.join("\n");
+    let prev;
+    while (prev !== map) {
+      prev = map;
+      map = map.replace(/^\..+>$|>\./gm, (m) =>
+        m.length === 2
+          ? ".>"
+          : ">".concat(m.slice(1, -1).replace(/>\./g, ".>"), ".")
+      );
+      const n = map.split("\n").map((p) => p.split(""));
+      const p = Array.from(n.map((m) => Array.from(m)));
+      p.forEach((line, x) => {
+        line.forEach((char, y) => {
+          if (char !== "v") return;
+          const pos = x === n.length - 1 ? p[0][y] : p[x + 1][y];
+          if (pos === ".") {
+            n[x === n.length - 1 ? 0 : x + 1][y] = "v";
+            n[x][y] = ".";
+          }
+        });
+      });
+      map = n.map((l) => l.join("")).join("\n");
+      count++;
+    }
+    return count;
+  },
+  {
+    sep: "\n",
+  }
+);
+task.expect(58);
+
+if (import.meta.main) await task.execute();
+
+export default task;
